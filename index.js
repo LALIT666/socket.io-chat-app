acf95636abf1c58eeae36e06c6328b6c356da891
LALIT666 <89864324+LALIT666@users.noreply.github.com>
2026-02-20 23:38:49 +0100

    new state

--- index.js
+++ index.js
@@ -3,20 +3,56 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  client_offset TEXT UNIQUE,
+  content TEXT
+  );
+  `);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+// io.on("connection", (socket) => {
+//   console.log("user connected");
+//   socket.broadcast.emit("hi");
+
+//   socket.on("chat message", (msg) => {
+//     io.emit("chat message", msg);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
+
 io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log("recovered?", socket.recovered);
+
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
